83fbf09aed52f5d8fe846b2f6351887e57096a79
Gaurav Sen <59478396+coding-parrot@users.noreply.github.com>
2022-08-16 16:20:36 +0530

    Rate Limiter Algorithms and LLD

---
+++ rate-limiter/src/main/java/models/Request.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Request {
+    private final String requestId;
+    private final long startTime;
+
+    public Request(String requestId, long startTime) {
+        this.requestId = requestId;
+        this.startTime = startTime;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return requestId.equals(((Request) o).requestId);
+    }
+
+    @Override
+    public int hashCode() {
+        return requestId.hashCode();
+    }
+}
